063156c89a7c84c03e9fe568591e1dade4d81c40
Drew <drew@drews-mbp-2.home>
2014-02-17 17:46:42 -0500

    Initial Commit

---
+++ UITableViewController Challenge Solution/ABViewController.h
@@ -0,0 +1,13 @@
+//
+//  ABViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Drew on 2/17/14.
+//  Copyright (c) 2014 BonVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABViewController : UIViewController
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Drew on 2/17/14.
+//  Copyright (c) 2014 BonVentures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ABAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ABAppDelegate class]));
+    }
+}
